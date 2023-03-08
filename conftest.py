403dbd3fd41b4fe628e5fd6639092b8e4ec9cf97
Vladyslav_Shevchenk3 <Vladyslav_Shevchenko3>
2023-03-09 00:03:29 +0200

    update tests

--- conftest.py
+++ conftest.py
@@ -1,32 +1,18 @@
 import pytest
 import requests
 
+from Api import Api
+from User import User
 from read_with_json_file import read_data
 
 
-@pytest.fixture(scope='session')
-def base_url():
-    return 'https://gorest.co.in/public/v2'
-
-
-@pytest.fixture(scope='session')
-def api_client():
-    bearer = 'a28db959a3da8d6d9a137120dd6c585e2cce6254441b4017fc3dd915300a38e3'
-    headers = {'Authorization': f'Bearer {bearer}', "accept": "application/json", "Content-Type": "application/json"}
-    client = requests.Session()
-    client.headers.update(headers)
-    return client
-
-
-@pytest.fixture(scope='session')
-def global_var():
-    return {"client_id": None}
+@pytest.fixture(scope="function")
+def user():
+    return User()
 
 
 @pytest.fixture(scope="function")
-def post_user_auto_delete(base_url, api_client):
-    endpoint = '/users'
-    response = api_client.post(url=base_url + endpoint, json=read_data('assert/good_user.json'))
+def post_user_auto_delete(user):
+    response = user.post_user(json=read_data('assert/good_user.json'))
     yield response
-    endpoint = f'/users/{response.json()["id"]}'
-    api_client.delete(url=base_url + endpoint)
+    user.delete_user(id_user=response.json()["id"])
--- test_user.py
+++ test_user.py
@@ -1,9 +1,8 @@
 from read_with_json_file import read_data
 
 
-def test_get_user_by_id(api_client, base_url):
-    endpoint = '/users'
-    response = api_client.get(url=base_url + endpoint, params={'page': 1, 'id': 577270})
+def test_get_user_by_id(user):
+    response = user.get_user(params={'page': 1, 'id': 892051})
     assert response.status_code == 200, response.json()
 
 
@@ -16,123 +15,116 @@ def test_post_user_auto_delete(post_user_auto_delete):
     assert post_user_auto_delete.json()['status'] == user['status'], post_user_auto_delete.json()
 
 
-class TestCreateEditDeleteUser:
-
-    def test_post_user(self, api_client, base_url, global_var):
-        user = read_data('assert/good_user.json')
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        global_var['client_id'] = response.json()['id']
-        assert response.status_code == 201, f'{response.json()}'
-        assert response.json()['name'] == user['name'], response.json()
-        assert response.json()['email'] == user['email'], response.json()
-        assert response.json()['gender'] == user['gender'], response.json()
-        assert response.json()['status'] == user['status'], response.json()
-
-    def test_post_existed_user(self, api_client, base_url):
-        user = read_data('assert/good_user.json')
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422
-        assert response.json()[0]['message'] == 'has already been taken'
-
-    def test_patch_user_name(self, api_client, base_url, global_var):
-        user = read_data('assert/good_user_update_name.json')
-        endpoint = '/users'
-        url = f'{base_url}{endpoint}/{global_var["client_id"]}'
-        response = api_client.patch(url=url, json=user)
-        assert response.status_code == 200
-        assert response.json()['name'] == user['name']
-
-    def test_delete_user(self, api_client, base_url, global_var):
-        endpoint = '/users'
-        url = f'{base_url}{endpoint}/{global_var["client_id"]}'
-        response = api_client.delete(url=url)
-        assert response.status_code == 204
+def test_post_existed_user(user):
+    user_data = read_data('assert/good_user.json')
+    response_post_user = user.post_user(user_data)
+    assert response_post_user.status_code == 201, f'{response_post_user.json()}'
+    response_post_existed_user = user.post_user(user_data)
+    assert response_post_existed_user.status_code == 422
+    assert response_post_existed_user.json()[0]['message'] == 'has already been taken'
+    user.delete_user(id_user=response_post_user.json()["id"])
+    assert response.status_code == 204
 
 
-class TestCreateUser:
+def test_patch_user_name(user):
+    user_data = read_data('assert/good_user.json')
+    response_post_user = user.post_user(user_data)
+    assert response_post_user.status_code == 201, f'{response_post_user.json()}'
+    user_new_data = read_data('assert/good_user_update_name.json')
+    response_patch_user = user.patch_user(id_user=response_post_user.json()["id"], json=user_new_data)
+    assert response_patch_user.status_code == 200
+    assert response_patch_user.json()['name'] == user_new_data['name']
+    user.delete_user(id_user=response_patch_user.json()["id"])
+    assert response.status_code == 204
 
-    def test_email_id_without_at_sign(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_without_at_sign.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
 
-    def test_email_id_without_domain_name(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_without_domain_name.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
+def test_delete_user(user):
+    user_data = read_data('assert/good_user.json')
+    response_post_user = user.post_user(user_data)
+    assert response_post_user.status_code == 201, f'{response_post_user.json()}'
+    response = user.delete_user(id_user=response_post_user.json()["id"])
+    assert response.status_code == 204
 
-    def test_email_id_without_top_level_domain(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_without_top_level_domain.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
 
-    def test_email_id_with_special_symbol(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_with_special_symbol.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
+def test_email_id_without_at_sign(user):
+    user_data = read_data("assert/user_email_id_without_at_sign.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
 
-    def test_email_id_with_multiple_at_signs(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_with_multiple_at_signs.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
 
-    def test_email_id_with_local_part(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_with_local_part.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
+def test_email_id_without_domain_name(user):
+    user_data = read_data("assert/user_email_id_without_domain_name.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
 
-    def test_email_id_start_with_a_dot(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_start_with_a_dot.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
 
-    def test_email_id_local_part_ends_with_a_dot(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_local_part_ends_with_a_dot.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
+def test_email_id_without_top_level_domain(user):
+    user_data = read_data("assert/user_email_id_without_top_level_domain.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
 
-    def test_email_id_with_multiple_dots(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_with_multiple_dots.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
 
-    def test_email_id_local_part_with_a_pass(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_local_part_with_a_pass.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
+def test_email_id_with_special_symbol(user):
+    user_data = read_data("assert/user_email_id_with_special_symbol.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
 
-    def test_email_id_domain_part_with_a_pass(self, api_client, base_url, global_var):
-        user = read_data("assert/user_email_id_domain_part_with_a_pass.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
 
-    def test_without_email_field(self, api_client, base_url, global_var):
-        user = read_data("assert/user_without_email_field.json")
-        endpoint = '/users'
-        response = api_client.post(url=base_url + endpoint, json=user)
-        assert response.status_code == 422, f'user data: {response.json()}'
-        assert response.json()[0]['message'] == 'is invalid'
+def test_email_id_with_multiple_at_signs(user):
+    user_data = read_data("assert/user_email_id_with_multiple_at_signs.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
+
+
+def test_email_id_with_local_part(user):
+    user_data = read_data("assert/user_email_id_with_local_part.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
+
+
+def test_email_id_start_with_a_dot(user):
+    user_data = read_data("assert/user_email_id_start_with_a_dot.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
+
+
+def test_email_id_local_part_ends_with_a_dot(user):
+    user_data = read_data("assert/user_email_id_local_part_ends_with_a_dot.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
+
+
+def test_email_id_with_multiple_dots(user):
+    user_data = read_data("assert/user_email_id_with_multiple_dots.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
+
+
+def test_email_id_local_part_with_a_pass(user):
+    user_data = read_data("assert/user_email_id_local_part_with_a_pass.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
+
+
+def test_email_id_domain_part_with_a_pass(user):
+    user_data = read_data("assert/user_email_id_domain_part_with_a_pass.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
+
+
+def test_without_email_field(user):
+    user_data = read_data("assert/user_without_email_field.json")
+    response = user.post_user(json=user_data)
+    assert response.status_code == 422, f'user data: {response.json()}'
+    assert response.json()[0]['message'] == 'is invalid'
